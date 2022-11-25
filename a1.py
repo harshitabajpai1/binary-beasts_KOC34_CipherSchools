ead10c5cf3b0516e14b0355d8702ca5983ff16aa
harshitabajpai1 <harshitabajpai2288@gmail.com>
2022-11-26 00:19:25 +0530

    first commit

---
+++ a1.py
@@ -0,0 +1,82 @@
+ 
+ # Python program to convert Roman Numerals to Numbers
+ 
+# This function returns value of
+# each Roman symbol
+def value(r):
+    if (r == 'I'):
+        return 1
+    if (r == 'V'):
+        return 5
+    if (r == 'X'):
+        return 10
+    if (r == 'L'):
+        return 50
+    if (r == 'C'):
+        return 100
+    if (r == 'D'):
+        return 500
+    if (r == 'M'):
+        return 1000
+    return -1
+str = str(input("ENTER A ROMAN NUMBER: "))
+def romanToDecimal(str):
+    res = 0
+    i = 0
+ 
+    while (i < len(str)):
+ 
+        # Getting value of symbol s[i]
+        s1 = value(str[i])
+ 
+        if (i + 1 < len(str)):
+ 
+            # Getting value of symbol s[i + 1]
+            s2 = value(str[i + 1])
+ 
+            # Comparing both values
+            if (s1 >= s2):
+ 
+                # Value of current symbol is greater
+                # or equal to the next symbol
+                res = res + s1
+                i = i + 1
+            else:
+ 
+                # Value of current symbol is greater
+                # or equal to the next symbol
+                res = res + s2 - s1
+                i = i + 2
+        else:
+            res = res + s1
+            i = i + 1
+ 
+    return res
+ 
+# Driver code
+print("Integer form of Roman Numeral is"),
+print(romanToDecimal(str))
+
+#  PROGRAMME TO COVERT NUMBER TO ROMAN NUMERALS
+# Function to convert integer to Roman values
+number = int(input("ENTER THE INTEGER: "))
+def printRoman(number):
+    num = [1, 4, 5, 9, 10, 40, 50, 90,
+        100, 400, 500, 900, 1000]
+    sym = ["I", "IV", "V", "IX", "X", "XL",
+        "L", "XC", "C", "CD", "D", "CM", "M"]
+    i = 12
+      
+    while number:
+        div = number // num[i]
+        number %= num[i]
+  
+        while div:
+            print(sym[i], end = "")
+            div -= 1
+        i -= 1
+  
+# Driver code
+if _name_ == "_main_":
+    print("Roman value is:", end = " ")
+    printRoman(number)    
--- python_project.py
+++ python_project.py
@@ -1,11 +1,16 @@
 from datetime import datetime
-try:
-    bday=input()
-    day1=datetime.strptime(bday,'%d/%m/%Y').date()
-    dday=input()
-    day2=datetime.strptime(dday,'%d/%m/%Y').date()
-    diff=(day2-day1)
+bday=input()
+day1=datetime.strptime(bday,'%d/%m/%Y').date()
+dday=input()
+day2=datetime.strptime(dday,'%d/%m/%Y').date()
+diff=(day2-day1)
+
+if diff.days>0:
     print("You lived",diff.days,"days")
-except:
-    print("You entered wrong date")
-print('THANK YOU FOR USING OUR APP')         
+else:
+    print('you entered wrong dates ')
+
+print('THANK YOU')        
+
+
+
